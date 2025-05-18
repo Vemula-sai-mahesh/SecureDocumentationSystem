5a13350661e663df69dec224fe05f53f9cabfc38
sai-mahesh-vemula <saimahesh@atparui.com>
2025-05-18 22:02:20 +0530

    git initial commit

---
+++ src/main/java/com/app/SecureDocumentationSystem/SecureDocumentationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.app.SecureDocumentationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDocumentationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDocumentationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/SecureDocumentationSystem/SecureDocumentationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SecureDocumentationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDocumentationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
